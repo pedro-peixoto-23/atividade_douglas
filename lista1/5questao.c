87e48a9970f392df5d425b0095965fa9ca9f1625
Pedro Peixoto Viana de Oliveira <85454019+pedro-peixoto-23@users.noreply.github.com>
2022-09-27 16:48:38 -0300

    quarta_alteracao

--- lista1/5questao.c
+++ lista1/5questao.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 int main() {
-	
+
     int escolha_usuario, quant_litros;
     float valor_final;
     const float valor_gasolina = 1.2, valor_alcool = 0.9;
-	
-	printf("Escolha uma das opcoes abaixo\n- 0. Alcool\n- 1. Gasolina\n\n");
-	printf("- Opcao desejada: ");
-	scanf("%d", &escolha_usuario);
-	
-	printf("- Quantidade de litros: ");
-	scanf("%d", &quant_litros);
-	
-	switch (escolha_usuario) {
-	    
+
+    printf("Escolha uma das opcoes abaixo\n- 0. Alcool\n- 1. Gasolina\n\n");
+    printf("- Opcao desejada: ");
+    scanf("%d", &escolha_usuario);
+
+    printf("- Quantidade de litros: ");
+    scanf("%d", &quant_litros);
+
+    switch (escolha_usuario) {
+
         case 0: // escolheu alcool
             if (quant_litros <= 20) {
-                valor_final = quant_litros * (valor_alcool * 0.97);
+            valor_final = quant_litros * (valor_alcool * 0.97);
             } else {
-                valor_final = quant_litros * (valor_alcool * 0.95);
+            valor_final = quant_litros * (valor_alcool * 0.95);
             }
         break;
 
         case 1: // escolheu gasolina
             if (quant_litros <= 20) {
-                valor_final = quant_litros * (valor_gasolina * 0.96);
+            valor_final = quant_litros * (valor_gasolina * 0.96);
             } else {
-                valor_final = quant_litros * (valor_gasolina * 0.94);
+            valor_final = quant_litros * (valor_gasolina * 0.94);
             }
         break;
 
