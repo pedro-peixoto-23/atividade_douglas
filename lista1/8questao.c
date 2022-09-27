10ed4c64b7fa85495c5292976ab977e9e46974df
Pedro Peixoto Viana de Oliveira <85454019+pedro-peixoto-23@users.noreply.github.com>
2022-09-27 17:49:53 -0300

    alteracao

--- lista1/8questao.c
+++ lista1/8questao.c
@@ -9,13 +9,13 @@ int main() {
 	
 	if (numero <= 0) {
 	    printf("\n- Voce digitou um numero invalido para este programa!");
-    } else {
-        printf("\n- Numeros: ");
-
-        for (int i = 1; i <= numero; i++) {
-        printf("%d ", i);
-        }
-    }
+	} else {
+	    printf("\n- Numeros: ");
+	
+            for (int i = 1; i <= numero; i++) {
+    	        printf("%d ", i);
+    	    }
+	}
  
 	return 0;
 }
